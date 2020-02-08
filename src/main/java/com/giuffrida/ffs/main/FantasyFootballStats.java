0ba4114f983e2afe6a8a953789841c9dfc7ed8a5
Joe Giuffrida <giuffridajoe@gmail.com>
2020-02-07 21:19:36 -0500

    first commit

---
+++ src/main/java/com/giuffrida/ffs/main/FantasyFootballStats.java
@@ -0,0 +1,9 @@
+package com.giuffrida.ffs.main;
+
+public class FantasyFootballStats 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
